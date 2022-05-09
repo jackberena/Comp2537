595f5034b730a5c074cd7b3649c16fb2dc5a1a07
Jack Berena <jberenaa@gmail.com>
2022-05-08 23:09:26 -0700

    worked on css

--- Assignment1/public/search.js
+++ Assignment1/public/search.js
@@ -1,35 +1,68 @@
 type_g = ""
-function processPokemonResp(data){
-    for(i=0; i < data.types.length; i++)
-        if(data.types[i].type.name == type_g){
-            $("main").append("<p>#" +data.id + "</p>")
-            $("main").append("<p>" +data.name + "</p>")
+
+function processPokemonResp(data) {
+    for (i = 0; i < data.types.length; i++)
+        if (data.types[i].type.name == type_g) {
+
+            $("main").append("<p>#" + data.id + "</p>")
+            $("main").append("<p>" + data.name + "</p>")
             $("main").append(`<div class = "image_container">
             <a href="/profile/${data.id}">
-             <img src="${data.sprites.other["official-artwork"].front_default}"></a> </div>`)
+             <img src="${data.sprites.other["official-artwork"].front_default}"></a></div>`)
+
         }
-    
+
 }
 
+function getbyname(data) {
+    $("main").append("<p>#" + data.id + "</p>")
+            $("main").append("<p>" + data.name + "</p>")
+            $("main").append(`<div class = "image_container">
+            <a href="/profile/${data.id}">
+             <img src="${data.sprites.other["official-artwork"].front_default}"></a>`)
+}
 
-function display(type_){
+        
+
+
+
+function display(type_) {
     $("main").empty();
     type_g = type_
-    for(i = 1 ; i < 851; i++){
+    for (i = 1; i < 851; i++) {
+
         $.ajax({
             type: "get",
             url: `https://pokeapi.co/api/v2/pokemon/${i}`,
             success: processPokemonResp
         })
     }
-    $("main").html()
-}
 
-function setup(){
+
+
+}
+$("main").html()
+
+
+function search_name() {
+    $("main").empty();
+    x = $("#pokemon_name").val()
+    $.ajax({
+        type: "get",
+        url: `https://pokeapi.co/api/v2/pokemon/${x}`,
+        success: getbyname
+    })
+}
+$("main").html()
+
+
+function setup() {
+    $("#get_pokemon").click(search_name)
     display($("#poke_type option:selected").val())
     $("#poke_type").change(() => {
         display($("#poke_type option:selected").val())
     })
+    
 }
 
 $(document).ready(setup)
