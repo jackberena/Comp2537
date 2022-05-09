984b6df1f76fdce8f40620fbcdb4fd1310643176
Jack Berena <jberenaa@gmail.com>
2022-05-08 20:39:55 -0700

    search by type

--- Assignment1/public/search.js
+++ Assignment1/public/search.js
@@ -1,10 +1,34 @@
+type_g = ""
+function processPokemonResp(data){
+    for(i=0; i < data.types.length; i++)
+        if(data.types[i].type.name == type_g){
+            $("main").append("<p>#" +data.id + "</p>")
+            $("main").append("<p>" +data.name + "</p>")
+            $("main").append(`<div class = "image_container">
+            <a href="/profile/${data.id}">
+             <img src="${data.sprites.other["official-artwork"].front_default}"></a> </div>`)
+        }
+    
+}
 
 
+function display(type_){
+    $("main").empty();
+    type_g = type_
+    for(i = 1 ; i < 851; i++){
+        $.ajax({
+            type: "get",
+            url: `https://pokeapi.co/api/v2/pokemon/${i}`,
+            success: processPokemonResp
+        })
+    }
+    $("main").html()
+}
 
 function setup(){
-
-    $("poke_type").change(()=>{
-        poke_type = $().val();
+    display($("#poke_type option:selected").val())
+    $("#poke_type").change(() => {
+        display($("#poke_type option:selected").val())
     })
 }
 
