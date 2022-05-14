e6127bbe9de6167b9667ec580845b494d02b5774
Jack Berena <jberenaa@gmail.com>
2022-05-13 23:16:39 -0700

    Assignment 2

--- Assignment1/server.js
+++ Assignment1/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.set('view engine', 'ejs')
 
-app.listen(5000, function(err){
+app.listen(process.env.PORT || 5000, function(err){
     if (err) 
         console.log(err);
 })
